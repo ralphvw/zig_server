13882b4f824d3a207984142afa76162f46a5781d
ralphvw <ralphvwilliams@icloud.com>
2024-09-16 07:35:20 +0000

    Methods

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,19 @@
 const std = @import("std");
 const Socket = @import("config.zig").Socket;
 const stdout = std.io.getStdOut().writer();
+const request = @import("request.zig");
 
 pub fn main() !void {
     const socket = try Socket.init();
     try stdout.print("Server address: {}\n", .{socket._address});
     var server = try socket._address.listen(.{});
     const connection = try server.accept();
-    _ = connection;
+    var buffer: [1024]u8 = undefined;
+
+    for (0..buffer.len) |i| {
+        buffer[i] = 0;
+    }
+
+    try request.read_request(connection, buffer[0..buffer.len]);
+    try stdout.print("{s}\n", .{buffer});
 }
--- src/request.zig
+++ src/request.zig
@@ -1,6 +1,13 @@
 const std = @import("std");
 const Connection = std.net.Server.Connection;
 
+pub const Method = enum {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+};
+
 pub fn read_request(conn: Connection, buffer: []u8) !void {
     const reader = conn.stream.reader();
     _ = try reader.read(buffer);
